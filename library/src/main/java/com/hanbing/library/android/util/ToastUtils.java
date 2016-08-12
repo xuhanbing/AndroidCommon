cc7cee637614401ddd40d877c86746a9b6d2243f
xuhanbing <xu_han_bing@163.com>
2016-08-12 18:10:22 +0800

    mod

--- library/src/main/java/com/hanbing/library/android/util/ToastUtils.java
+++ library/src/main/java/com/hanbing/library/android/util/ToastUtils.java
@@ -4,6 +4,7 @@
 package com.hanbing.library.android.util;
 
 import android.content.Context;
+import android.os.Handler;
 import android.os.Looper;
 import android.widget.Toast;
 
@@ -12,7 +13,7 @@ import android.widget.Toast;
  * @date 2015-6-17
  */
 public class ToastUtils {
-
+    static Toast mToast;
     /**
      * 显示toast
      *
@@ -29,21 +30,39 @@ public class ToastUtils {
      * @param context
      * @param text
      */
-    public static void showToast(Context context, String text) {
-        if (null != context) {
-
+    public static void showToast(final Context context, final String text) {
+        if(null != context) {
             boolean isMainThread = isMainThread(context);
+            if(isMainThread) {
+                show(context, text);
+            } else {
 
-            if (!isMainThread)
-                Looper.prepare();
+                Handler handler = new Handler(context.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        show(context, text);
+                    }
+                });
 
-            Toast.makeText(context.getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-
-            if (!isMainThread) {
-                Looper.loop();
             }
-            ;
+
         }
+    }
+    private static void show(Context context, String text) {
+        cancel();
+
+        Toast toast = createToast(context, text);
+        toast.show();
+        mToast = toast;
+    }
+
+    private static Toast createToast(Context context, String text) {
+        return Toast.makeText(context.getApplicationContext(), text, Toast.LENGTH_SHORT);
+    }
+
+    private static void cancel() {
+        if (null != mToast) mToast.cancel();
     }
 
     public static boolean isMainThread(Context context) {
